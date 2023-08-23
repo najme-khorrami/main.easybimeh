a29cad3958f0b9e2c780f6e31ed71119f284429d
najme-khorrami <najme@gmail.com>
2023-08-23 16:05:13 +0330

    first commit

---
+++ easybimeh/src/layouts/MainLayout.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <!-- contact header -->
+    <div class="contact-header bg-secondary">
+      <div class="container">
+        <a href="#">
+          <q-icon name="fab fa-linkedin-in" size="20px" color="info" class="q-pa-sm"></q-icon>
+        </a>
+        <a href="#">
+          <q-icon name="fab fa-telegram-plane" size="xs" color="info" class="q-pa-sm"></q-icon>
+        </a>
+        <a href="#">
+          <q-icon name="fab fa-instagram" size="xs" color="info" class="q-pa-sm"></q-icon>
+        </a>
+        <a href="#">
+          <q-icon name="fab fa-twitter" size="xs" color="info" class="q-pa-sm"></q-icon>
+        </a>
+      </div>
+
+      
+    </div>
+    <!-- menu header -->
+    <div class="menu-header bg-primary">
+
+    </div>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  setup () {
+
+    return {
+
+    }
+  }
+})
+</script>
+<style lang="scss" scoped>
+  .contact-header {
+    height: 35px;
+  }
+  .menu-header {
+    height: 75px;
+    position: sticky;
+    top: 0;
+  }
+</style>
---
+++ easybimeh/src/pages/IndexPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center">
+    <transition appear enter-active-class="animated bounceInLeft" leave-active-class="animated bounceOutRight">
+      <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+
+    </transition>
+    <q-icon name="warning" />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
