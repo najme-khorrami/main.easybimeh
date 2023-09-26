961e2cd269b801eeaae0469072d3f2ceb5655972
najme-khorrami <n.khorrami8819@gmail.com>
2023-09-26 17:00:41 +0330

    complete eb layout

--- easybimeh/src/layouts/EBLayout.vue
+++ easybimeh/src/layouts/EBLayout.vue
@@ -1,23 +1,36 @@
 <template>
     <q-layout view="lHh Lpr lFf">
         <!-- menu header -->
-        <div class="menu-header row items-center shadow-1">
+        <div class="menu-header row items-center shadow-3">
             <div class="container full-width full-height">
                 <q-toolbar class="full-width full-height justify-between" style="padding: 0;">
                     <q-btn flat class="btn--no-hover q-pa-none q-ma-none full-height" to="/">
-                        <q-img src="../../src/assets/easybimeh-logo-white.svg" alt="ایزی بیمه" width="180px"
-                            style="min-width:130px;" fit="fill"></q-img>
+                        <q-img src="../../src/assets/easybimeh-logo-white.svg" alt="ایزی بیمه" width="160px" fit="contain"></q-img>
                     </q-btn>
                     <div class="tabs full-height gt-sm row no-wrap justify-between items-center">
-                        <div v-for="item in tabList" :key="item.id">
-                            <a @click="showRequestDialog = (item.id == 4) ? true : false;" :href="item.href"
-                                class="block text-white q-pa-md" style="font-size: 14px;white-space: nowrap;">{{ item.title
-                                }}</a>
+                        <div v-for="item in headerList" :key="item.id">
+                            <a :href="item.link" class="block text-white q-px-md q-py-sm" style="font-size: 14px;white-space: nowrap;">{{ item.title }}</a>
                         </div>
                     </div>
+                    <q-img src="../../src/assets/eb-connect.svg" width="200px" alt="eb-connect" fit="contain"></q-img>
                 </q-toolbar>
             </div>
         </div>
+
+        <q-page-container>
+            <router-view />
+        </q-page-container>
+
+        <!-- copy right section -->
+        <section id="copy-right" class="text-center text-white q-pa-md text-weight-light">
+            <span class="secondry">{{ year }}</span>
+            <span class="secondry"> © </span>
+            <span>
+                تمام حقوق این نرم افزار متعلق به 
+                <a href="https://www.aka.co.ir/" class="secondry" target="_blank">شرکت معماران عصر دانش</a>
+                می باشد.
+            </span>
+        </section>
     </q-layout>
 </template>
 
@@ -29,14 +42,15 @@ export default defineComponent({
 
     data() {
         return {
-            // headerList: [
-            //     {id:1 ,title: 'محصولات اصلی',link: ''},
-            //     {id:2 ,title: 'مزیت‌های رقابتی',link: ''},
-            //     {id:3 ,title: 'وایت لیبل ایبیکانکت',link: ''},
-            //     {id:4 ,title: 'امکانات مدیریتی و کنترلی',link: ''},
-            //     {id:5 ,title: 'ارائه دهنده API برای صنعت بیمه',link: ''},
-            //     {id:6 ,title: '',link: ''}
-            // ]
+            headerList: [
+                {id:1 ,title: 'محصولات اصلی',link: '#'},
+                {id:2 ,title: 'مزیت‌های رقابتی',link: '#'},
+                {id:3 ,title: 'وایت لیبل ایبیکانکت',link: '#'},
+                {id:4 ,title: 'امکانات مدیریتی و کنترلی',link: '#'},
+                {id:5 ,title: 'ارائه دهنده API برای صنعت بیمه',link: '#'},
+                {id:6 ,title: 'ارائه سرویس های بیمه',link: '#'}
+            ],
+            year: '2023',
         }
     }
 })
@@ -44,8 +58,26 @@ export default defineComponent({
 
 <style lang="scss" scoped>
     $bg-header: #2e39bf;
+    $bg-footer: #0a0d35;
+    $secondry-color: #09EBAF;
     .menu-header {
         background-color: $bg-header;
         height: 72px;
+        position: sticky;
+        top: 0;
+        z-index: 2;
+        .tabs {
+            margin: 0 10px;
+            a:hover {
+                background-color: rgba(#6e6e6e, .2);
+            }
+        }
+    }
+    #copy-right {
+        background-color: $bg-footer;
+        font-size: 16px;
+        .secondry {
+            color: $secondry-color;
+        }
     }
 </style>
--- easybimeh/src/pages/EBIndexPage.vue
+++ easybimeh/src/pages/EBIndexPage.vue
@@ -1,11 +1,89 @@
 <template>
+    <q-page>
+        <section id="main-banner" class="q-py-lg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-xs-12">
+                        <q-img src="../../src/assets/isometric.svg" fit="contain" style="max-width: 600px;"></q-img>
+                    </div>
+                    <div class="col-md-6 col-xs-12">
+                        <q-img src="../../src/assets/Assetss.svg" fit="contain" style="max-width: 80px;"></q-img>
+                        <p>
+                            <h1>EasyBimehConnect</h1>
+                            یک ساب برند از ایزی بیمه است که وظیفه ارائه خدمات
+                            <span>B2B</span>
+                            را بر پایه 
+                            <span>API</span>
+                            و
+                            <span>B2B2C</span>
+                            را بر پایه 
+                            <span>وایت لیبل </span>
+                            بر عهده دارد. اگر اپلیکیشن و یا سایت غیر بیمه‌ای دارید و تمایل به فروش بیمه نامه دارید از امروز میتوانید با کمترین هزینه و کمترین زمان به زنجیره نوآوری در صنعت بیمه متصل شوید و تجارت جدیدی بسازید. 
+                        </p>
+                        <a href="#">
+                            شماره تماس : 02191691049 
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="main-products">
+            <div class="products">
 
+            </div>
+        </section>
+    </q-page>
 </template>
-
+  
 <script>
+import { defineComponent } from 'vue'
 
+export default defineComponent({
+    name: 'EBIndexPage',
+
+    data() {
+        return {
+
+        }
+    }
+})
 </script>
-
-<style>
-
+  
+<style lang="scss" scoped>
+    $bg-header: #2e39bf;
+    $bg-footer: #0a0d35;
+    $secondry-color: #09EBAF;
+    #main-banner {
+        background: url('../../src/assets/bg.svg') no-repeat center;
+        background-size: cover;
+        h1 {
+            font-size: 32px;
+            color: $secondry-color;
+            display: inline;
+            line-height: 0;
+        }
+        p {
+            text-align: justify;
+            color: #fff;
+            font-size: 24px;
+            span {
+                color: #feb155;
+            }
+        }
+        a {
+            text-decoration: underline;
+            color: #fff;
+            font-size: 24px;
+        }
+    }
+    #main-products {
+        margin: auto;
+        .products {
+            background-color: #fff;
+            border-radius: 30px;
+        }
+    }
+    div {
+        border: 1px solid red;
+    }
 </style>
